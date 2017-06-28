9f051868461825713abb993ec39078d8f90d52c9
Tiffany Monroe <tiffanymonroe@Tiffanys-MacBook-Pro.local>
2017-06-27 22:16:01 -0700

    Commit 9 - Created loops.js file

---
+++ unit_1/w01d02/homework/loops.js
@@ -0,0 +1,21 @@
+for (let a=0; a <= 99; a++)
+  console.log(a);
+
+for (let b=99; b >= 0; b--)
+  console.log(b);
+
+for (let c=0; c <= 98; c+=2)
+console.log(c)
+
+for (let d=98; d >= 0; d-=2)
+console.log(d);
+
+for (let e=49; e <= 72; e++)
+console.log(e);
+
+for (let f=81; f >= 26; f--)
+console.log(f);
+//confused on how you ascend from 81-26.
+
+for (let g=3; g <= 90; g*=3)
+console.log(g);
