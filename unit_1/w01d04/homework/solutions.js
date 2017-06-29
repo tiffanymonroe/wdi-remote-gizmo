51b1ae638d16f51d25eaf816b1930f52aa593802
Tiffany Monroe <tiffanymonroe@Tiffanys-MacBook-Pro.local>
2017-06-29 16:24:13 -0700

    Commit 2 - Palindrome

--- unit_1/w01d04/homework/solutions.js
+++ unit_1/w01d04/homework/solutions.js
@@ -10,3 +10,16 @@
       // func(argument);
 
 // Return shows the value of the data; console.log() prints out data in our command line.
+
+//  Palindrome
+
+// const checkPalindrome = (string) => {
+//   const reversedString = string.split('').reverse().join('');
+//   return string.toLowerCase() === reversedString.toLowerCase();
+// }
+//
+// console.log(checkPalindrome("Radar"));
+// console.log(checkPalindrome("Borscht"));
+
+// ** most of the code from my class notes **
+// ** toLowerCase from Stack overflow **
