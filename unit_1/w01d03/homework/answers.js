74c0043e816a4f437fd6530541b250d89fd4c27b
Tiffany Monroe <tiffanymonroe@Tiffanys-MacBook-Pro.local>
2017-06-28 17:20:25 -0700

    Commit 3 - Excited Kittens answered

--- unit_1/w01d03/homework/answers.js
+++ unit_1/w01d03/homework/answers.js
@@ -4,6 +4,24 @@
 // }
 
 //.......Get Even..........
-for (b=0; b <= 200; b+=2){
-  console.log(b);
+// for (b=0; b <= 200; b+=2){
+//   console.log(b);
+// }
+
+//......Excited Kitten......
+
+//1.
+//
+// for (c=1; c <= 20; c++)
+// console.log("Love me, pet me! HSSSSSS!");
+
+//2.
+//help from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+let getRandom = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+
+for (d=1; d <= 20; d++)
+if (d % 2 === 0) {
+  console.log(getRandom[0])
 }
+// confused about Math.random()
