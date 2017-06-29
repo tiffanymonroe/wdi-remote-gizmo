f42589a2cbdc2e4290ab081a6f5327897a1e4ebe
Tiffany Monroe <tiffanymonroe@Tiffanys-MacBook-Pro.local>
2017-06-29 16:09:26 -0700

    Commit 1 - Verbal questions

---
+++ unit_1/w01d04/homework/solutions.js
@@ -0,0 +1,12 @@
+//  Functions
+//.......verbal questions.......
+
+// An argument is input of the function; the parameter is what the function will do with that information.
+
+      // const func (parameter) => {
+      //   code block
+      // };
+      //
+      // func(argument);
+
+// Return shows the value of the data; console.log() prints out data in our command line.
