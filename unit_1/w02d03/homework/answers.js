e2e00e287e801edd8e600030369386f39cdfc141
Tiffany Monroe <tiffanymonroe@Tiffanys-MacBook-Pro.local>
2017-07-05 16:49:52 -0700

    2nd commit - created some data structures and accessed some data

--- unit_1/w02d03/homework/answers.js
+++ unit_1/w02d03/homework/answers.js
@@ -6,3 +6,18 @@
 // 4. array
 // 5. object
 // 6. object
+
+//  TAKE IT EASY
+
+const rainbow = ["red", "orange", "yellow", "green", "blue", "purple", "violet"];
+
+console.log(rainbow[4]);
+
+const tiffany = {
+  favFood: "sushi",
+  hobby: "reading",
+  currentTown: "OC. Don't call it that.",
+  favDataType: "objects!"
+}
+
+console.log(tiffany.hobby);
