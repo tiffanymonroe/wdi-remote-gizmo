aacc3adb4cf20e820f66a09d0f35cc8ca3b82710
Tiffany Monroe <tiffanymonroe@Tiffanys-MacBook-Pro.local>
2017-07-06 20:50:57 -0700

    Commit 2 - Combine objects, arrays, and functions more than one level deep

--- unit_1/w02d04/homework/answers.js
+++ unit_1/w02d04/homework/answers.js
@@ -28,3 +28,45 @@
 // ];
 //
 // array[2]();
+
+// ======================================================
+
+//Create a function that returns a function. Call that inner function
+
+// const functionCeption = ()=>{
+//   return ()=>{
+//     console.log("It's Funct-ion-ception! :O");
+//   }
+// };
+//
+// functionCeption()();
+
+//Create an array that has a function that returns an object that has an array. Log an element of the inner array.
+
+// const array = [
+//     1,
+//     2,
+//     ()=>{
+//       return {
+//         innerArray: [3,4, "Found me!"]
+//   }
+//   }
+// ];
+//
+//   array[2]().innerArray[2];
+
+//Create an array that has a function that returns a function. Call the inner function.
+
+// const array = [
+//   "hello",
+//   "I'm part of an array.",
+//   ()=> {
+//     return ()=>{
+//         console.log("I'm kind of getting this!");
+//     }
+//   }
+// ];
+// 
+// array[2]()();
+
+//======================================================
