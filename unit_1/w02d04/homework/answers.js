4bd9a6301c6cb8ce674559f55d4bad9ce6c3e643
Tiffany Monroe <tiffanymonroe@Tiffanys-MacBook-Pro.local>
2017-07-06 20:26:17 -0700

    Commit 1 - Combine objects, arrays, and functions

---
+++ unit_1/w02d04/homework/answers.js
@@ -0,0 +1,30 @@
+// Create an object that has a property that is an array. Log one of the elements of that array.
+
+// const obj = {
+//   array: [1,2,3]
+// }
+//
+// console.log(obj.array[1]);
+
+//Create an object that has a property that is a function (method). Call that method.
+
+// const obj = {
+//   method() {
+//       console.log('thanks for grading my homework!');
+// }
+// }
+//
+//
+// obj.method();
+
+//Create an array that has a function as one of its elements. Call that function.
+
+// const array = [
+//   1,
+//   2,
+//   ()=> {
+//     console.log("I'm an awesome function!");
+//   }
+// ];
+//
+// array[2]();
