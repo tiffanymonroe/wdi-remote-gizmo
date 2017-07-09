ce7f43dd315393dba6b5b88848f25b7a322057bb
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-08 17:52:44 -0700

    Commit 2 - It's all about Me done

--- unit_1/w02d05/homework/object_answers.js
+++ unit_1/w02d05/homework/object_answers.js
@@ -1,17 +1,40 @@
 //Clicker
 
-const clicker = {
-  clickCount: 0,
-  click: ()=>{
-    clicker.clickCount++;
-    return clicker.clickCount;
-//I needed help figuring out the method after office hours, so I looked at Sarah Anderson's repository and found clicker.clickCount++; and return clicker.clickCount. I couldn't remember how to do it! I kept trying a for loop, but it wouldn't work.
-  }
-};
+// const clicker = {
+//   clickCount: 0,
+//   click: ()=>{
+//     clicker.clickCount++;
+//     return clicker.clickCount;
+// //I needed help figuring out the method after office hours, so I looked at Sarah Anderson's repository and found clicker.clickCount++; and return clicker.clickCount. I couldn't remember how to do it! I kept trying a for loop, but it wouldn't work.
+//   }
+// };
+//
+// clicker.click();
+// clicker.click();
+// clicker.click();
+// console.log("The click count is " + clicker.clickCount);
 
-clicker.click();
-clicker.click();
-clicker.click();
-console.log("The click count is " + clicker.clickCount);
+//It's all about me
+
+const myInformation = {
+  name: "Tiffany",
+  age: 36,
+  fact: "I have two Cavalier King Charles Spaniels.",
+  statement: ()=>{
+    console.log("My name is " + myInformation.name +". I'm " + myInformation.age + " years old and " + myInformation.fact + " In 10 years, I'll be 46 years old. Eek!");
+  },
+  ageIncrease: ()=> {
+    myInformation.age++;
+    console.log ("Happy Birthday to me! I'm now " + myInformation.age + ".");
+  },
+  nameChange: ()=> {
+    myInformation.name = "Rumpelstiltskin";
+    console.log("My new name is " + myInformation.name + ".");
+  }
+}
+
+myInformation.statement();
+myInformation.ageIncrease();
+myInformation.nameChange();
 
 //
