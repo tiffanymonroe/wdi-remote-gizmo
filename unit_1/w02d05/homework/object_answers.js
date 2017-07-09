895c97df88350ad5dfb7d50e0b5daa16ce4d89d7
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-08 19:35:56 -0700

    Commit 3 - Greeter done

--- unit_1/w02d05/homework/object_answers.js
+++ unit_1/w02d05/homework/object_answers.js
@@ -16,25 +16,44 @@
 
 //It's all about me
 
-const myInformation = {
-  name: "Tiffany",
-  age: 36,
-  fact: "I have two Cavalier King Charles Spaniels.",
-  statement: ()=>{
-    console.log("My name is " + myInformation.name +". I'm " + myInformation.age + " years old and " + myInformation.fact + " In 10 years, I'll be 46 years old. Eek!");
+// const myInformation = {
+//   name: "Tiffany",
+//   age: 36,
+//   fact: "I have two Cavalier King Charles Spaniels.",
+//   statement: ()=>{
+//     console.log("My name is " + myInformation.name +". I'm " + myInformation.age + " years old and " + myInformation.fact + " In 10 years, I'll be 46 years old. Eek!");
+//   },
+//   ageIncrease: ()=> {
+//     myInformation.age++;
+//     console.log ("Happy Birthday to me! I'm now " + myInformation.age + ".");
+//   },
+//   nameChange: ()=> {
+//     myInformation.name = "Rumpelstiltskin";
+//     console.log("My new name is " + myInformation.name + ".");
+//   }
+// }
+//
+// myInformation.statement();
+// myInformation.ageIncrease();
+// myInformation.nameChange();
+
+//Greeter
+
+
+const greeter = {
+  hello: (name)=>{
+    console.log("Hey, " + name + ".");
   },
-  ageIncrease: ()=> {
-    myInformation.age++;
-    console.log ("Happy Birthday to me! I'm now " + myInformation.age + ".");
+  goodbye: (name)=>{
+    console.log("Goodbye, " + name + ".");
   },
-  nameChange: ()=> {
-    myInformation.name = "Rumpelstiltskin";
-    console.log("My new name is " + myInformation.name + ".");
+  whoAreYou: (name)=>{
+    console.log("Oh right! " + name +  " how could i forget...!");
   }
 }
 
-myInformation.statement();
-myInformation.ageIncrease();
-myInformation.nameChange();
+greeter.hello("Jude");
+greeter.goodbye("Earl");
+greeter.whoAreYou("Rumpelstiltskin");
 
 //
