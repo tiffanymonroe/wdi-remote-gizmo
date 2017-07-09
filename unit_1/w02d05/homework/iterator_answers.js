cffab73a6c8804ffa528807cce445392a6f29563
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-08 23:30:34 -0700

    Commit 9 - map done

--- unit_1/w02d05/homework/iterator_answers.js
+++ unit_1/w02d05/homework/iterator_answers.js
@@ -1,9 +1,16 @@
 // forEach
 
-const words = ["Ho", "Chi", "Minh", "City", "was", "once", "known", "as", "Prey", "Nokor"];
+// const words = ["Ho", "Chi", "Minh", "City", "was", "once", "known", "as", "Prey", "Nokor"];
+//
+// words.forEach((word)=> {
+//   console.log(words);
+// });
 
-words.forEach((word)=> {
-  console.log(words);
+// Map
+
+const more_words = ["Joe", "Overeats", "Eggs"];
+const newWord = more_words.map((word)=>{
+      console.log(word.charAt(0));
 });
 
-//
+// I missed something, but not sure what. I can print the letters, not the letters as a new array.
