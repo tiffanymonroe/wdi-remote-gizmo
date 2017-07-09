3126194f427edcb584c9bbce100384ea6417f686
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-08 23:42:48 -0700

    Commit 10 - reduce done

--- unit_1/w02d05/homework/iterator_answers.js
+++ unit_1/w02d05/homework/iterator_answers.js
@@ -8,9 +8,21 @@
 
 // Map
 
-const more_words = ["Joe", "Overeats", "Eggs"];
-const newWord = more_words.map((word)=>{
-      console.log(word.charAt(0));
-});
+// const more_words = ["Joe", "Overeats", "Eggs"];
+// const newWord = more_words.map((word)=>{
+//       console.log(word.charAt(0));
+// });
 
 // I missed something, but not sure what. I can print the letters, not the letters as a new array.
+
+// reduce
+
+const arr = [8, 8, 8, 8, 8, 8, 8, 8];
+
+const product = arr.reduce((a, b)=>{
+    return a * b;
+}, 1);
+
+console.log(product);
+
+//
