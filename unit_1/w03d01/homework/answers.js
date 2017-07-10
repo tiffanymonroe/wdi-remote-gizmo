06c1c9e106dfde1b0e9c1f616b67387da2386006
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-10 16:17:28 -0700

    Commit 2 - Created Dog Class

--- unit_1/w03d01/homework/answers.js
+++ unit_1/w03d01/homework/answers.js
@@ -1,9 +1,29 @@
 class Pet {
   constructor(name, owner){
     this.name = name;
-    this.owner = "";
+    this.owner = owner;
   }
   setOwner(owner){
     this.owner = owner;
   }
 }
+
+class Dog extends Pet {
+  constructor(name, owner, price){
+    super(name, owner);
+    this.price = price;
+  }
+  bark(){
+    console.log("bark");
+  }
+  chaseTail(){
+    console.log("oh boy oh boy oh boy");
+  }
+  getPrice(){
+    return "$" + this.price
+  }
+}
+
+const sparky = new Dog('Sparky', 'Victor', 150);
+console.log(sparky);
+console.log(sparky.getPrice());
