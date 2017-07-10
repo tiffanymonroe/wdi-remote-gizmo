e682c57444bd5f9344643404a1e96d20fdfdaa34
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-10 16:22:29 -0700

    Commit 3 - Created Cat Class

--- unit_1/w03d01/homework/answers.js
+++ unit_1/w03d01/homework/answers.js
@@ -27,3 +27,23 @@ class Dog extends Pet {
 const sparky = new Dog('Sparky', 'Victor', 150);
 console.log(sparky);
 console.log(sparky.getPrice());
+
+class Cat extends Pet {
+  constructor(name, owner, price){
+    super(name, owner);
+    this.price = price
+  }
+  purr(){
+    console.log("purrrrr");
+  }
+  clean(){
+    console.log("cleaning");
+  }
+  getPrice(){
+    return "$" + this.price
+  }
+}
+
+const sprinkles = new Cat ('Sprinkles', 'Weird Girl', 75)
+console.log(sprinkles);
+console.log(sprinkles.getPrice());
