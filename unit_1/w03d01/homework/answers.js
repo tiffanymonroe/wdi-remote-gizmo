4d89c35345f0984c0bbae042c2275ca3492c6377
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-10 20:07:46 -0700

    Commit 4 - Created Person Class

--- unit_1/w03d01/homework/answers.js
+++ unit_1/w03d01/homework/answers.js
@@ -8,6 +8,8 @@ class Pet {
   }
 }
 
+//======================================================
+
 class Dog extends Pet {
   constructor(name, owner, price){
     super(name, owner);
@@ -28,6 +30,9 @@ const sparky = new Dog('Sparky', 'Victor', 150);
 console.log(sparky);
 console.log(sparky.getPrice());
 
+
+//======================================================
+
 class Cat extends Pet {
   constructor(name, owner, price){
     super(name, owner);
@@ -47,3 +52,54 @@ class Cat extends Pet {
 const sprinkles = new Cat ('Sprinkles', 'Weird Girl', 75)
 console.log(sprinkles);
 console.log(sprinkles.getPrice());
+
+//======================================================
+
+class Person {
+  constructor(name, age, weight, mood, pets, bankAccount){
+    this.name = name;
+    this.age = 0;
+    this.weight = 0;
+    this.mood = 0;
+    this.pets = [];
+    this.bankAccount = 0;
+  }
+  getName(){
+    return this.name
+  }
+  getAge(){
+    return this.age
+  }
+  getWeight(){
+    return this.weight
+  }
+  greet(other_person){
+    console.log("Hello, " + other_person);
+  }
+  eat(){
+    this.weight++;
+    this.mood++;
+  }
+  exercise(){
+    this.weight--;
+  }
+  age(){
+    this.age++;
+    this.weight++;
+    this.mood--;
+    this.bankAccount+=10;
+  }
+  buyPet(pet){
+    this.pets.push(pet);
+    this.mood+=10;
+    // this.bankAccount - Pet.getPrice();
+  }
+}
+
+const jill = new Person ("Jill");
+jill.age = 42;
+jill.weight = 140;
+jill.mood = 10;
+jill.pets = "Marshmallow";
+jill.bankAccount = 5000;
+console.log(jill);
