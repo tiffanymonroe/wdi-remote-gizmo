b54b87e0b5ceb85fba4d1fcaaf04fa1575cc4324
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-10 22:45:21 -0700

    Commit 6 - Layout Practice

--- unit_1/w03d01/student_examples/classes/factories.js
+++ unit_1/w03d01/student_examples/classes/factories.js
@@ -1,25 +1,53 @@
-class Car {
-  constructor(seriallNumber){
-    this.seriallNumber = seriallNumber
-  }
-  drive(){
-    console.log("Vroom");
+// class Car {
+//   constructor(seriallNumber){
+//     this.seriallNumber = seriallNumber
+//   }
+//   drive(){
+//     console.log("Vroom");
+//   }
+// }
+//
+// const factory = {
+//   cars: 0,
+//   generateCar(){
+//     const newCar = new Car(this.cars);
+//     this.cars++;
+//     return newCar;
+//   },
+//   findCar(index){
+//     return this.cars[index];
+//   }
+// }
+//
+// const firstCar = factory.generateCar();
+// const secondCar = factory.generateCar();
+//
+// console.log(firstCar);
+// console.log(secondCar);
+
+//==========================
+class Factory {
+  constructor (name){
+    this.name = name;
+    this.computers = [];
   }
 }
 
-const factory = {
-  cars: 0,
-  generateCar(){
-    const newCar = new Car(this.cars);
-    this.cars++;
-    return newCar;
-  },
-  findCar(index){
-    return this.cars[index];
+class Computer {
+  constructor (serialNumber){
+    this.serialNumber = serialNumber;
+  }
+    generateComputer(){
+      const newComputer = new Computer(this.name, this.computers.length);
+      this.computers.push(newComputer)
+      return newComputer;
+  }
+  findComputer(index){
+    return this.computers[index];
   }
 }
 
-const firstCar = factory.generateCar();
-const secondCar = factory.generateCar();
 
-console.log(factory);
+const mac = new Factory('MacBook Pro');
+mac.generateComputer();
+console.log(mac.findComputer(0));
