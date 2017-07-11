e2f85ef09fc86587d4f92f42dacac163fa11d022
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-11 09:14:19 -0700

    initial game setup

---
+++ unit_1/w03d02/student_labs/spacebattle/apps.js
@@ -0,0 +1,23 @@
+// console.log("That's no moon!");
+
+const USS_Schwarzenegger = {
+  hull: 20,
+  firepower: 5,
+  accuracy: .7,
+  // attack(target) => {
+  //   if (Math.random() > USS_Schwarzenegger[0].attack){
+  //     console.log("You hit the ship!");
+  //   }
+  // }
+}
+
+const alien_one = {
+  hull: [3, 4, 5, 6],
+  firepower: [2, 3, 4],
+  accuracy: [.6, .7, .8]
+}
+
+// attack();
+
+console.log(USS_Schwarzenegger);
+console.log(alien_one);
