5aefdf4c39ac6961a68e14f5c4436c7bb29f72db
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-12 17:23:13 -0700

    Commit 1 - I added my first element and it was fun

--- unit_1/w03d03/homework/hogwarts/app.js
+++ unit_1/w03d03/homework/hogwarts/app.js
@@ -1,3 +1,16 @@
-if (typeof jQuery == 'undefined'){
-  console.log('oops! I still have to link my jQuery properly!');
-} else {console.log('I did it! I linked jQuery and this js file!')};
+// if (typeof jQuery == 'undefined'){
+//   console.log('oops! I still have to link my jQuery properly!');
+// } else {console.log('I did it! I linked jQuery and this js file!')};
+
+$(() => {
+
+  const $container = $('div');
+  console.log($container);
+
+  const $h1 = $('<h1>');
+  console.log($h1);
+
+  $h1.text('Hogwarts');
+  $container.append($h1);
+
+});
