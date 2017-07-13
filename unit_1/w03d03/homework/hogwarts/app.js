cd4cd6e95f1778a7d03f908e47216110627055df
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-12 17:42:52 -0700

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- unit_1/w03d03/homework/hogwarts/app.js
+++ unit_1/w03d03/homework/hogwarts/app.js
@@ -9,8 +9,24 @@ $(() => {
 
   const $h1 = $('<h1>');
   console.log($h1);
-
   $h1.text('Hogwarts');
   $container.append($h1);
+
+  const $name = $('<h2>');
+  $name.text('Tiffany');
+  $container.append($name);
+
+  const $house = $('<h3>');
+  $house.text('House: Hufflepuff');
+  $container.append($house);
+
+  const $pet = $('<h4>');
+  $pet.addClass('pet');
+  $pet.text('Pet: Tink, Dog')
+  $container.append($pet);
+
+  const $wand = $('<h4>');
+  $wand.text('Wand: Hornbeam with Dragon Heartstring Core');
+  $container.append($wand);
 
 });
