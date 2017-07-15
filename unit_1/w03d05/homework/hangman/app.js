be188d02fbe621eb10ce1d24921af3c30835210a
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-15 07:48:16 -0700

    hangman setup

--- unit_1/w03d05/homework/hangman/app.js
+++ unit_1/w03d05/homework/hangman/app.js
@@ -1,10 +1,24 @@
 // console.log("hello, it's me");
 $(()=> {
+
+//Global scope
 const words = ["line", "stanza", "meter", "rhyme", "sound"];
 
 const chooseWord = () => {
   const randomIndex = Math.floor(Math.random() * (6 - 0)) + 0;
   return words[randomIndex];
+}
+
+//Local scope
+
+const game = {
+  guessesRemaining: ,
+  guessedLetters: ,
+  isOver(){
+
+  },
+  overMessage:
+
 }
 
 console.log(chooseWord());
