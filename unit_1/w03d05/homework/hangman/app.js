f956eff13fc89a05ff2b961f555fd3151fffe024
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-15 08:28:03 -0700

    1st commit

--- unit_1/w03d05/homework/hangman/app.js
+++ unit_1/w03d05/homework/hangman/app.js
@@ -9,19 +9,50 @@ const chooseWord = () => {
   return words[randomIndex];
 }
 
+
+
 //Local scope
 
-const game = {
-  guessesRemaining: ,
-  guessedLetters: ,
-  isOver(){
+// const game = {
+//   guessesRemaining: 0,
+//   guessedLetters: [],
+//   isOver(){
+//
+//   },
+//   overMessage:
+//
+// }
 
-  },
-  overMessage:
+// const Letter = {
+//   constructor(value, hidden) {
+//     this.value = "";
+//     this.hidden = true;
+//   }
+//
+//   show(){
+//     this.hidden = false;
+//   }
+//   display(){
+//     if (this.hidden = false){
+//       return
+//     }
+//     else return "_"
+//   }
+// }
 
+const Word = {
+  // constructor(getLetters, letters){
+  //   this.getLetters = getLetters;
+  //   this.letters = letters;
+  // },
+  getLetters(newWord){
+    letters = newWord.split().push()
+    // letters.push();
+  }
 }
 
-console.log(chooseWord());
+
+
 
 
 }); //end window.onload
