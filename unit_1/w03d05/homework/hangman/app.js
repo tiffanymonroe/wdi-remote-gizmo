c502a5441aed4e79ce0f2469c2a02b559f90cd79
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-15 16:26:04 -0700

    global scope runs

--- unit_1/w03d05/homework/hangman/app.js
+++ unit_1/w03d05/homework/hangman/app.js
@@ -9,49 +9,10 @@ const chooseWord = () => {
   return words[randomIndex];
 }
 
+// console.log(chooseWord());
 
 
 //Local scope
-
-// const game = {
-//   guessesRemaining: 0,
-//   guessedLetters: [],
-//   isOver(){
-//
-//   },
-//   overMessage:
-//
-// }
-
-// const Letter = {
-//   constructor(value, hidden) {
-//     this.value = "";
-//     this.hidden = true;
-//   }
-//
-//   show(){
-//     this.hidden = false;
-//   }
-//   display(){
-//     if (this.hidden = false){
-//       return
-//     }
-//     else return "_"
-//   }
-// }
-
-const Word = {
-  // constructor(getLetters, letters){
-  //   this.getLetters = getLetters;
-  //   this.letters = letters;
-  // },
-  getLetters(newWord){
-    letters = newWord.split().push()
-    // letters.push();
-  }
-}
-
-
 
 
 
