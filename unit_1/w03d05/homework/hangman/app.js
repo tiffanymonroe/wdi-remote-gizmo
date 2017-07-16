93708f134511d2b5bc0c3bce879edb868181cf63
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-15 23:44:21 -0700

    letter submission event handler works

--- unit_1/w03d05/homework/hangman/app.js
+++ unit_1/w03d05/homework/hangman/app.js
@@ -19,14 +19,57 @@ const game = {
   guessedLetters: []
 }
 
+const Letter = {
+  constructor(value, hidden){
+    this.value = value;
+    this.hidden = true;
+  },
+  show(){
+    return this.hidden = false;
+  },
+  display(){
+    if (this.hidden === true){
+      return "_"
+    }
+    else
+    return chooseWord.charAt(chooseWord[i]).val
+  },
+  test(letter){
+    for (let i=0; i < chooseWord.length; i++)
+    if (letter === chooseWord().charAt(chooseWord()[i]).val){
+      show();
+    }
+    else {
+      console.log("this letter doesn't match any in the word");
+    }
+  }
+}
+
+console.log(Letter.hidden);
+
 const startGame = () => {
   game.guesses = 10,
-  guessedLetters = [""],
+  guessedLetters = [],
   chooseWord();
 }
 
 startGame();
 
-$('#word').append(chooseWord())
+Letter.test('x');
+
+$('#word').append(chooseWord());
+
+
+//Event listeners
+
+$('input').on('keypress', ()=> {
+  console.log('the key had been pressed');
+  $('button').on('click', ()=>{
+    let letter = $('input').val();
+    console.log(letter);
+  })
+});
+
+
 
 }); //end window.onload
