dc31c164fdbbe83b836d8a2b8fb53e1ac632648c
Matt Huntington <matt.huntington@gmail.com>
2017-07-17 15:08:08 -0400

    updating w6d1 complete example

--- unit_2/w06d01/instructor_examples/fruits_complete/server.js
+++ unit_2/w06d01/instructor_examples/fruits_complete/server.js
@@ -1,37 +1,37 @@
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-var db = mongoose.connection;
-var bodyParser = require('body-parser');
-var Fruits = require('./models/fruits.js');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+const bodyParser = require('body-parser');
+const Fruits = require('./models/fruits.js');
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/fruits/new', function(req, res){
+app.get('/fruits/new', (req, res)=>{
     res.render('new.ejs');
 });
 
-app.post('/fruits/', function(req, res){
+app.post('/fruits/', (req, res)=>{
     if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
         req.body.readyToEat = true;
     } else { //if not checked, req.body.readyToEat is undefined
         req.body.readyToEat = false;
     }
-    Fruits.create(req.body, function(){
+    Fruits.create(req.body, ()=>{
         res.redirect('/fruits');
     });
 });
 
-app.get('/fruits', function(req, res){
-    Fruits.find({}, function(error, allFruits){
+app.get('/fruits', (req, res)=>{
+    Fruits.find({}, (error, allFruits)=>{
         res.render('index.ejs', {
             fruits: allFruits
         });
     });
 });
 
-app.get('/fruits/:id', function(req, res){
-    Fruits.findById(req.params.id, function(err, foundFruit){
+app.get('/fruits/:id', (req, res)=>{
+    Fruits.findById(req.params.id, (err, foundFruit)=>{
         res.render('show.ejs', {
             fruit:foundFruit
         });
@@ -39,10 +39,10 @@ app.get('/fruits/:id', function(req, res){
 });
 
 mongoose.connect('mongodb://localhost:27017/basiccrud');
-db.once('open', function() {
+db.once('open', ()=> {
     console.log('connected to mongo');
 });
 
-app.listen(3000, function(){
+app.listen(3000, ()=>{
     console.log('listening');
 });
