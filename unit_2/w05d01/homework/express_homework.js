419440a3a1fa005349454b30a9c8d80cbec57c70
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-24 22:00:09 -0700

    Commit 3: Magic 8 Ball express application created.

--- unit_2/w05d01/homework/express_homework.js
+++ unit_2/w05d01/homework/express_homework.js
@@ -14,14 +14,29 @@ const app = express();
 
 //I'm really confused by what we're supposed to do with this one.
 
-const tip = (total, tipPercentage) => {
-    console.log("I'm confused");
-}
+// const tip = (total, tipPercentage) => {
+//     console.log("I'm confused");
+// }
+//
+// app.get('/tip/:total/:tipPercentage', (req, res) => {
+//   res.send(tip[req.params.total.tipPercentage]);
+// });
 
-app.get('/tip/:total/:tipPercentage', (req, res) => {
-  res.send(tip[req.params.total.tipPercentage]);
+
+//Magic 8 Ball
+
+const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+//
+
+let magic = Math.floor(Math.random() * responses.length) + 0;
+
+
+
+app.get('/magic', (req, res) => {
+    res.send(magic[req.params])
 });
 
+//it runs, but nothing is printing out.
 
 app.listen(3000, () => {
   console.log('listening');
