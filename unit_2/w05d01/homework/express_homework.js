9b6d0ae6021dabe6fe2535a9bb9b6e525a3594f4
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-24 21:00:02 -0700

    Commit 2: Tip Calculator express application created.

--- unit_2/w05d01/homework/express_homework.js
+++ unit_2/w05d01/homework/express_homework.js
@@ -1,11 +1,27 @@
-//Greetings
-
 const express = require('express');
 const app = express();
 
-app.get('/greeting/:name', (req, res) => {
-  res.send("Hello")
+//Greetings
+
+// app.get('/greeting/:name', (req, res) => {
+//   res.send("Hello")
+// });
+//
+
+
+
+//Tip Calculator
+
+//I'm really confused by what we're supposed to do with this one.
+
+const tip = (total, tipPercentage) => {
+    console.log("I'm confused");
+}
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+  res.send(tip[req.params.total.tipPercentage]);
 });
+
 
 app.listen(3000, () => {
   console.log('listening');
