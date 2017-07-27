b2e07f1ac447a6647d79cb7756289af5caa3d6d2
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-27 15:14:37 -0700

    Server is working and displays a plain index page

---
+++ unit_2/w05d04/homework/pokedex_app/server.js
@@ -0,0 +1,34 @@
+// DEPENDENCIES
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
+
+// DATA
+const pokemon = require('./models/pokemon.js')
+
+// LISTENER
+
+app.listen(3000, () => {
+  console.log("Gotta catch 'em all!");
+})
+
+// MIDDLEWARE
+
+//static files
+app.use(express.static('public'));
+
+//parse information when we create new items
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//view data before parsing
+app.get('/models', (req, res) => {
+  res.send(pokemon);
+});
+
+//-------Index Route-------
+app.get('/pokemon', (req, res) => {
+  res.render('index.ejs', {pokemon: pokemon});
+});
