1548c1343786144e9192ea9f6f3a2dc52648b191
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-27 22:38:19 -0700

    Has the ability to add a new Pokémon--but won't post

--- unit_2/w05d04/homework/pokedex_app/server.js
+++ unit_2/w05d04/homework/pokedex_app/server.js
@@ -38,6 +38,41 @@ app.get('/pokemon/new', (req, res) => {
   res.render('new.ejs');
 });
 
+//--------Create Route-------
+//from Homework solutions
+
+app.post('pokemon/', (req, res) => {
+  const newPokemon = req.body;
+  console.log(newPokemon);
+
+const formatType = (type) => {
+  const typeArray = type.split(",");
+  return typeArray
+}
+
+const formatDamages = (damages) => {
+  const damageObject = {};
+  console.log(damageObject);
+
+  const damageArray = damages.split(",");
+  console.log(damageArray);
+
+  const finalDamages = [];
+  for (const damage in damageArray){
+    finalDamages.push(damageArray[damage].split(":"));
+    formattedDamages[finalDamages[damage][0]] = finalDamages[damage][1]
+  }
+  return formattedDamages;
+}
+
+newPokemon.type = formatType(newPokemon.type);
+newPokemon.damages = formatDamages(newPokemon.damages);
+
+pokemon.push(newPokemon);
+res.redirect('/pokemon/' + (pokemon.length-1));
+
+});
+
 //-------Show Route-------
 app.get('/pokemon/:id', (req, res) => {
   res.render('show.ejs', {
