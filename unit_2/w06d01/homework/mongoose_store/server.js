94fb2ee05ad045e506c30dd914cd8199ee409c73
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-31 21:33:16 -0700

    Show working

--- unit_2/w06d01/homework/mongoose_store/server.js
+++ unit_2/w06d01/homework/mongoose_store/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const Product = require('./models/products.js');
 const productData = require('./models/portola.js');
-console.log(productData);
+
 
 
 //Middleware
