59585346c0287ec15b7420a6d378535b3f62fe0e
Tiffany Monroe <hello@tiffanymonroe.com>
2017-08-01 18:17:55 -0700

    Delete Working

---
+++ unit_2/w06d01/homework/mongoose_store/public/app.js
@@ -0,0 +1,10 @@
+const name = product[i].
+
+const createModal = (name, price) =>{
+
+}
+
+//event listener
+$(.images).on('hover' ()=> {
+
+})
--- unit_2/w06d01/homework/mongoose_store/server.js
+++ unit_2/w06d01/homework/mongoose_store/server.js
@@ -3,13 +3,15 @@ const express = require('express');
 const app = express();
 const Product = require('./models/products.js');
 const productData = require('./models/portola.js');
-
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
 
 
 //Middleware
-const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
+app.use(methodOverride('_method'));
 
 //Index Route
 
@@ -43,6 +45,32 @@ app.get('/shop/:id', (req, res) => {
   });
 });
 
+
+
+//Delete Route
+app.delete('/shop/:id', (req, res) => {
+  Product.findByIdAndRemove(req.params.id, (error, data) => {
+    res.redirect('/shop');
+  });
+});
+
+//Edit Route
+
+app.get('/shop/:id/edit', (req, res) => {
+  Product.findById(req.params.id, (error, foundProduct) => {
+    res.render('edit.ejs',{
+      product: foundProduct
+    });
+  });
+});
+
+//Put Route
+app.put('/shop/:id', (req, res) => {
+  Product.findBrsyIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedModel) => {
+    res.send(updatedModel);
+  });
+});
+
 //Connect to MongoDB
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/shop');
@@ -50,7 +78,7 @@ mongoose.connection.once('open', () => {
   console.log('connected to mongo');
 })
 
-//Add products
+// Add products
 // Product.collection.insertMany(productData, (error, data) => {
 //   console.log('added real Portola coffee');
 //   mongoose.connection.close();
