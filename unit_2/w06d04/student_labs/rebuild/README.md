618565747727dfec8c18e3d62c17e2349ac51ab7
Matt Huntington <matt.huntington@gmail.com>
2017-08-03 14:43:21 -0400

    afternoon lab

---
+++ unit_2/w06d04/student_labs/rebuild/README.md
@@ -0,0 +1,3 @@
+# Lab
+
+Go through [today's notes](../../instructor_notes/BUILD.md) and replay the steps in [this directory](blog/)
---
+++ unit_2/w06d04/student_labs/rebuild/blog/models/authors.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const authorSchema = mongoose.Schema({
+	name: String
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+module.exports = Author;
