c8c45caa152c0d6c07b018a307a04f4e1c7a3e2d
Tiffany Monroe <hello@tiffanymonroe.com>
2017-08-03 07:50:53 -0700

    morning exercise

---
+++ unit_2/w06d04/morning_exercise/bubble_sort.js
@@ -0,0 +1,13 @@
+
+
+const bubbleSort = (array) => {
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array){
+      console.log("less than");
+    }
+  }
+};
+
+
+bubbleSort([10, 0, 99, 100, 8, 55, 2, 17, 16, 15, 1000, 1]);
