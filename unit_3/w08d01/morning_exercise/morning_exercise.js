6b0c72ef291fae3f0f606bc629084ae5dde93682
Tiffany Monroe <hello@tiffanymonroe.com>
2017-08-14 08:09:27 -0700

    morning exercise

---
+++ unit_3/w08d01/morning_exercise/morning_exercise.js
@@ -0,0 +1,11 @@
+const validCard = (num) => {
+  const sum = 0;
+  const array = num.toString().split('');
+      console.log(array);
+  for (let i=0; i < array.length; i-=2){
+
+
+  }
+
+
+console.log(validCard(1234567890123456));
