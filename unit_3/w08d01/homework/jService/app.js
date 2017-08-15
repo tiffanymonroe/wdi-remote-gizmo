34a76dd9d85ab1823ee7dbe99858d6a733e0bcbe
Tiffany Monroe <hello@tiffanymonroe.com>
2017-08-14 21:53:36 -0700

    commit 10: the data is stored

--- unit_3/w08d01/homework/jService/app.js
+++ unit_3/w08d01/homework/jService/app.js
@@ -1,5 +1,21 @@
 // console.log('Huzzah! app.js is connected for Angular homework');
 
 const app = angular.module("TriviaApp", [function(){
+}]);
 
+app.controller("TriviaController", ['$http', function($http){
+
+  this.question = function(){
+  $http({
+    method: "GET",
+    url: 'http://jservice.io/api/random'
+  }).then(
+      function(response){
+        console.log(response);
+      },
+      function(err){
+        console.log(err);
+      }
+    );
+  }
 }]);
