b292278302ce1c1c36d60d4a77032488dbe943fc
Jim Haff <Jimmyhaff3@gmail.com>
2017-08-15 21:48:50 -0700

    Added student examples

---
+++ unit_3/w08d03/student_examples/weather_api/public/app.js
@@ -0,0 +1,20 @@
+const app = angular.module('myApp', []);
+
+app.controller('BaseCtrl', ['$http', function($http){
+    console.log(' this is happening ')
+    const controller = this;
+    this.getTweets = function() {
+        $http({
+            method: 'GET',
+            url: '/tweets/getTweets'
+        }).then(
+            function(response){
+                console.log(response, ' this is response')
+
+            },
+            function(err){
+                console.log(err);
+            }
+        );
+    }
+}])
