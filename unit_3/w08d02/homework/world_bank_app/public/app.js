66c1ae021f729f97ed7965f885764e6150dede47
Tiffany Monroe <hello@tiffanymonroe.com>
2017-08-15 21:16:09 -0700

    Commit 3: Count of database records appears on the page

--- unit_3/w08d02/homework/world_bank_app/public/app.js
+++ unit_3/w08d02/homework/world_bank_app/public/app.js
@@ -1,5 +1,18 @@
+// console.log('connected to worldbank');
 const app = angular.module('worldbank', []);
 
 app.controller('mainController', ['$http', function($http){
-  
-}])
+  const controller = this;
+  this.displayData = function(){
+      $http({
+        method: 'GET',
+        url: '/wbinfo/count'
+      }).then(function(response){
+        controller.count = response.data;
+      }, function(err){
+        console.log(err);
+      }
+    );
+  }
+  this.displayData();
+}]);
