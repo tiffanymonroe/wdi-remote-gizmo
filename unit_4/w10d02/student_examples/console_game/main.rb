adc4e5d186d216e58436740201988e9078ad5826
Tiffany Monroe <hello@tiffanymonroe.com>
2017-08-29 12:51:03 -0700

    cleaning up class work

---
+++ unit_4/w10d02/student_examples/console_game/main.rb
@@ -0,0 +1,54 @@
+# a word is chosen from words
+# a message is displayed giving a clue
+# user can enter a guess
+# result??
+
+# indefinite loop
+
+dictionary = ["toothpicks", "cloth", "lamp",
+"soup", "scissors", "headphones", "donkeys"]
+
+class Word
+
+	def initialize word
+		letters = word.split('')
+		@letters_display = letters.map do |l|
+			{ letter: l, hidden: true }
+		end
+	end
+
+	def letters_display
+		@letters_display
+	end
+
+	def render
+		@letters_display.each do |l|
+			if l[:hidden]
+				print " _ "
+			else
+				print l.letter
+			end
+		end
+	end
+
+end
+
+word = Word.new dictionary.sample
+
+loop do
+
+	puts "Guess a letter!"
+	word.render
+	input = gets.chomp
+
+	if word.letters_display.include?(input)
+		puts "yeahhhh you got it."
+		break
+	else
+		puts "WRONG"
+		puts "press enter or (q)uit"
+		confirm = gets.chomp
+		break if confirm == "q"
+	end
+
+end
