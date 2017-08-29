22187cac01ece3edd386b8360ddf96a0c1226379
Tiffany Monroe <hello@tiffanymonroe.com>
2017-08-28 20:08:35 -0700

    Commit 2: Arrays

--- unit_4/w10d01/homework/ruby.rb
+++ unit_4/w10d01/homework/ruby.rb
@@ -1,11 +1,25 @@
-p 'hello world'
+# p 'hello world'
+#
+# adjective = 'Big Bad'
+#
+# p "Hello #{adjective} World"
+#
+# greeting = 'hello world'
+#
+# p greeting.upcase!
+#
+# p greeting
 
-adjective = 'Big Bad'
+nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
 
-p "Hello #{adjective} World"
+p nums.uniq
+p nums.push(17)
+p nums.pop
+p nums.length
+p nums.include? 8
 
-greeting = 'hello world'
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-p greeting.upcase!
-
-p greeting
+p colors.sample
+p colors.reverse
+colors.each {|num| p num.capitalize }
