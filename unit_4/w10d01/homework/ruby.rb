aed84b3a722ba3745a4f17d017561f2db28320e5
Tiffany Monroe <hello@tiffanymonroe.com>
2017-08-28 20:42:06 -0700

    Commit 3: Methods

--- unit_4/w10d01/homework/ruby.rb
+++ unit_4/w10d01/homework/ruby.rb
@@ -10,16 +10,32 @@
 #
 # p greeting
 
+# nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
+#
+# p nums.uniq
+# p nums.push(17)
+# p nums.pop
+# p nums.length
+# p nums.include? 8
+#
+# colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+#
+# p colors.sample
+# p colors.reverse
+# colors.each {|num| p num.capitalize }
+
+
+def find_area height, width
+  height * width
+end
+
+p find_area(3,7)
+
 nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
 
-p nums.uniq
-p nums.push(17)
-p nums.pop
-p nums.length
-p nums.include? 8
 
-colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+def multiply_each_by_five arr
+  arr * 5
+end
 
-p colors.sample
-p colors.reverse
-colors.each {|num| p num.capitalize }
+nums.each_index {|i| p multiply_each_by_five nums[i]}
