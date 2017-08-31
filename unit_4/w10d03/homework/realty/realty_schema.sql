3bebb2203d8aca06e3227b81938ed327d8cb1cb5
Tiffany Monroe <hello@tiffanymonroe.com>
2017-08-30 22:53:04 -0700

    Commit: REALTY - Realty schema created

--- unit_4/w10d03/homework/realty/realty_schema.sql
+++ unit_4/w10d03/homework/realty/realty_schema.sql
@@ -1,1 +1,24 @@
 -- enter your schema for apartments, offices and storefronts below
+
+CREATE TABLE  apartments (
+  id serial primary key,
+  apt_number integer,
+  beds integer,
+  bath integer,
+  address varchar(255) not null,
+  tenant varchar(255),
+  occupied_status boolean,
+  sqft integer,
+  price integer
+);
+
+CREATE TABLE storefront (
+  id serial primary key,
+  address varchar(255),
+  occupied_status boolean,
+  price integer,
+  kitchen boolean,
+  sqft integer,
+  owner varchar(255),
+  outdoor_seating boolean default false,
+);
