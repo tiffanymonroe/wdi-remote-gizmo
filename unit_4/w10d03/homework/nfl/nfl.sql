a5f561cacc51f9edc55301c0aaee3eb3ba0cb62e
Tiffany Monroe <hello@tiffanymonroe.com>
2017-08-30 21:45:01 -0700

    Commit: NFL - I seeded the database with the players and teams info

--- unit_4/w10d03/homework/nfl/nfl.sql
+++ unit_4/w10d03/homework/nfl/nfl.sql
@@ -1,5 +1,45 @@
+-- 0. Seeded Data
+-- \i nfl_seed_data/teams.SQL
+-- \i nfl_seed_data/players.SQL
+
 -- 1. List the names of all NFL teams'
 
+SELECT name FROM teams;
+
+--          name
+-- ----------------------
+--  Buffalo Bills
+--  Miami Dolphins
+--  New England Patriots
+--  New York Jets
+--  Baltimore Ravens
+--  Cincinnati Bengals
+--  Cleveland Browns
+--  Pittsburgh Steelers
+--  Houston Texans
+--  Indianapolis Colts
+--  Jacksonville Jaguars
+--  Tennessee Titans
+--  Denver Broncos
+--  Kansas City Chiefs
+--  Oakland Raiders
+--  San Diego Chargers
+--  Dallas Cowboys
+--  New York Giants
+--  Philadelphia Eagles
+--  Washington Redskins
+--  Chicago Bears
+--  Detroit Lions
+--  Green Bay Packers
+--  Minnesota Vikings
+--  Atlanta Falcons
+--  Carolina Panthers
+--  New Orleans Saints
+--  Tampa Bay Buccaneers
+--  Arizona Cardinals
+--  St. Louis Rams
+--  San Francisco 49ers
+--  Seattle Seahawks
 
 -- 2. List the stadium name and head coach of all NFC teams
 
