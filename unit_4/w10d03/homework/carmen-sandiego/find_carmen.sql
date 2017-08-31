e729a956153feddfcb42aeca6a73b45ba045fc8f
Tiffany Monroe <hello@tiffanymonroe.com>
2017-08-30 21:14:23 -0700

    I caught Carmen Sandiego

--- unit_4/w10d03/homework/carmen-sandiego/find_carmen.sql
+++ unit_4/w10d03/homework/carmen-sandiego/find_carmen.sql
@@ -25,31 +25,92 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
 
+SELECT MIN(population) FROM country WHERE region = 'Southern Europe';
 
+SELECT * FROM country WHERE region = 'Southern Europe' AND population = 1000;
+
+-- code |             name              | continent |     region      | surfacearea | indepyear | population | lifeexpectancy | gnp  | gnpold |            localname            |      governmentform      |     headofstate     | capital | code2
+------+-------------------------------+-----------+-----------------+-------------+-----------+------------+----------------+------+--------+---------------------------------+--------------------------+---------------------+---------+-------
+ -- VAT  | Holy See (Vatican City State) | Europe    | Southern Europe |         0.4 |      1929 |       1000 |                | 9.00 |        | Santa Sede/Citt� del Vaticano | Independent Church State | Johannes Paavali II |    3538 | VA
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
+SELECT * FROM countrylanguage WHERE countrycode = 'VAT';
 
+-- countrycode | language | isofficial | percentage
+-- -------------+----------+------------+------------
+-- VAT         | Italian  | t          |          0
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
+
+SELECT * FROM countrylanguage WHERE language = 'Italian';
+
+-- countrycode | language | isofficial | percentage
+-- -------------+----------+------------+------------
+--  ITA         | Italian  | t          |       94.1
+--  SMR         | Italian  | t          |        100
+--  VAT         | Italian  | t          |          0
+--  ARG         | Italian  | f          |        1.7
+--  AUS         | Italian  | f          |        2.2
+--  LIE         | Italian  | f          |        2.5
+--  BEL         | Italian  | f          |        2.4
+--  BRA         | Italian  | f          |        0.4
+--  LUX         | Italian  | f          |        4.6
+--  MCO         | Italian  | f          |       16.1
+--  CHE         | Italian  | t          |        7.7
+--  CAN         | Italian  | f          |        1.7
+--  FRA         | Italian  | f          |        0.4
+--  DEU         | Italian  | f          |        0.7
+--  USA         | Italian  | f          |        0.6
+
+SELECT * FROM country WHERE code = 'SMR';
+
+--  code |    name    | continent |     region      | surfacearea | indepyear | population | lifeexpectancy |  gnp   | gnpold | localname  | governmentform | headofstate | capital | code2
+-- ------+------------+-----------+-----------------+-------------+-----------+------------+----------------+--------+--------+------------+----------------+-------------+---------+-------
+--  SMR  | San Marino | Europe    | Southern Europe |          61 |       885 |      27000 |           81.1 | 510.00 |        | San Marino | Republic       |             |    3171 | SM
 
 
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
+SELECT * FROM city WHERE countrycode = 'SMR';
+
+-- SEARCHED city first to see what the columns were.
+
+--   id  |    name    | countrycode |     district      | population
+-- ------+------------+-------------+-------------------+------------
+--  3170 | Serravalle | SMR         | Serravalle/Dogano |       4802
+--  3171 | San Marino | SMR         | San Marino        |       2294
 
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+SELECT * FROM city WHERE name LIKE '%Serra%';
 
+--   id  |         name         | countrycode |     district      | population
+-- ------+----------------------+-------------+-------------------+------------
+--   265 | Serra                | BRA         | Esp�rito Santo  |     302666
+--   310 | Tabo�o da Serra    | BRA         | S�o Paulo       |     197550
+--   370 | Itapecerica da Serra | BRA         | S�o Paulo       |     126672
+--  3170 | Serravalle           | SMR         | Serravalle/Dogano |       4802
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
 
+SELECT * FROM country WHERE name = 'Brazil';
 
+-- code |  name  |   continent   |    region     | surfacearea | indepyear | population | lifeexpectancy |    gnp    |  gnpold   | localname |  governmentform  |        headofstate        | capital | code2
+-- ------+--------+---------------+---------------+-------------+-----------+------------+----------------+-----------+-----------+-----------+------------------+---------------------------+---------+-------
+-- BRA  | Brazil | South America | South America |  8.5474e+06 |      1822 |  170115000 |           62.9 | 776739.00 | 804108.00 | Brasil    | Federal Republic | Fernando Henrique Cardoso |     211 | BR
+
+SELECT * FROM city WHERE id = 211;
+
+--  id  |    name    | countrycode |     district     | population
+-- -----+------------+-------------+------------------+------------
+--  211 | Bras�lia | BRA         | Distrito Federal |    1969868
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
@@ -66,7 +127,12 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+SELECT * FROM city  WHERE population = 91084;
+
+--  id  |     name     | countrycode |  district  | population
+-- ------+--------------+-------------+------------+------------
+-- 4060 | Santa Monica | USA         | California |      91084
 
 
 
--- She's in ____________________________!
+-- She's by me in Santa Monica!
