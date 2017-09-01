4bdd3df31cc4d2536d161c3f7f17ea81a945621a
Tiffany Monroe <hello@tiffanymonroe.com>
2017-09-01 12:37:37 -0700

    Commit 4: Created model and seeded db

--- unit_4/w10d05/homework/tweeter_app_api/db/seeds.rb
+++ unit_4/w10d05/homework/tweeter_app_api/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+2.times do
+  Tweet.create({
+    title: Faker::StarWars.planet,
+    content: Faker::StarWars.quote,
+    author: Faker::StarWars.character
+    })
+end
