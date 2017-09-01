bdedc7ffae157414327712b24be44127f32ca33d
Kristyn Bryan <kristynrb@gmail.com>
2017-09-01 11:08:29 -0400

    updated morning exercise walkthrough

--- unit_4/w10d05/instructor_examples/scraper.rb
+++ unit_4/w10d05/instructor_examples/scraper.rb
@@ -1,21 +1,24 @@
 require "open-uri"
 require "Nokogiri"
 
+# Getting data from the Internet
 # doc = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_original_programs_distributed_by_Netflix"))
 
-doc = Nokogiri::HTML(File.open("netflix_data_practice.html"))
-
-# puts tableData = doc.css("table")
-
-# puts tableDataText = doc.css("table").text
-
-# p allTitles = doc.css("a").text
-
-allTitles = doc.css('li').each do |item|
-title = item.text
-if title != ""
-  puts "Title is: #{title}"
-end
-end
+# Getting data from a file
+doc = Nokogiri::HTML(File.open("netflix_data.html"))
 
 # puts doc
+
+# tableData = doc.css("table").text
+#
+# puts tableData
+
+allTitles = doc.css("a").text
+# puts allTitles
+
+doc.css("a").each do |item|
+  title = item.text
+  if title != ""
+    puts "Title is: #{title}"
+  end
+end
