c147d4fe55a4916ec1430489937ae134f5e040b5
Tiffany Monroe <hello@tiffanymonroe.com>
2017-09-01 12:27:34 -0700

    Commit 3: Successfully run the Tweets migration

--- unit_4/w10d05/homework/tweeter_app_api/db/migrate/20170901192133_create_tweets.rb
+++ unit_4/w10d05/homework/tweeter_app_api/db/migrate/20170901192133_create_tweets.rb
@@ -1,6 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[5.1]
   def change
     create_table :tweets do |t|
+      t.string "title"
+      t.text "content"
+      t.string "author"
     end
   end
 end
