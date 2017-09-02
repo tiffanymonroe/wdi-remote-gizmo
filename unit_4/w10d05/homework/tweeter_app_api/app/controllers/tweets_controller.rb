df986a5031283c6fa3c8730f3653cb7694a76e0a
Tiffany Monroe <hello@tiffanymonroe.com>
2017-09-01 17:39:07 -0700

    Commit 7: Tweet show

--- unit_4/w10d05/homework/tweeter_app_api/app/controllers/tweets_controller.rb
+++ unit_4/w10d05/homework/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -8,7 +8,7 @@ class TweetsController < ApplicationController
   #show route
   def show
     one_tweet = Tweet.find(params[:id])
-    render json: {tweet: one_tweet}
+    render json: {status: 200, tweet: one_tweet}
   end
 
 end
