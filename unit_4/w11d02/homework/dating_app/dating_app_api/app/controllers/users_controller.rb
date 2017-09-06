4754160c5f1bbe0ba7657fd7a46e5e833ce8ccbc
Tiffany Monroe <hello@tiffanymonroe.com>
2017-09-05 21:25:06 -0700

    Commit 6: Displayed users with Angular

--- unit_4/w11d02/homework/dating_app/dating_app_api/app/controllers/users_controller.rb
+++ unit_4/w11d02/homework/dating_app/dating_app_api/app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
+    @users = User.all.sample(6)
 
     render json: @users
   end
--- unit_4/w11d02/homework/dating_app/dating_app_frontend/public/app.js
+++ unit_4/w11d02/homework/dating_app/dating_app_frontend/public/app.js
@@ -3,11 +3,15 @@ const app = angular.module('dating_app', []);
 app.controller('mainController', ['$http', function($http){
 
   this.message = console.log('main controller works!');
-
+  this.users = [];
   $http({
     method: 'get',
     url: 'http://localhost:3000/users'
-  }).then(res => console.log(res))
+  }).then(res => {
+    console.log(res)
+    this.users = res.data
+  })
+
     .catch(err => console.log(err))
 
 }]) //end of controller
