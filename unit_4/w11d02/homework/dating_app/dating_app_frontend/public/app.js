29cba7c44d77bb4565444a8c4f7b76d05566aa91
Tiffany Monroe <hello@tiffanymonroe.com>
2017-09-05 20:29:40 -0700

    Commit 4: Configured CORS

--- unit_4/w11d02/homework/dating_app/dating_app_frontend/public/app.js
+++ unit_4/w11d02/homework/dating_app/dating_app_frontend/public/app.js
@@ -2,6 +2,8 @@ const app = angular.module('dating_app', []);
 
 app.controller('mainController', ['$http', function($http){
 
+  this.message = console.log('main controller works!');
+
   $http({
     method: 'get',
     url: 'http://localhost:3000/users'
