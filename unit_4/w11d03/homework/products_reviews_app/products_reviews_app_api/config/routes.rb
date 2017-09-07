d1a707a731901649d0210ffcd59301e6f8649df0
Tiffany Monroe <hello@tiffanymonroe.com>
2017-09-06 20:23:57 -0700

    Commit 8: Routes changed for Reviews update and delete

--- unit_4/w11d03/homework/products_reviews_app/products_reviews_app_api/config/routes.rb
+++ unit_4/w11d03/homework/products_reviews_app/products_reviews_app_api/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   resources :products, only: [:index, :show] do
-    resources :reviews, except: [:show]
+    resources :reviews, except: [:show, :update, :destroy]
   end
+
+  resources :reviews, only: [:update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
