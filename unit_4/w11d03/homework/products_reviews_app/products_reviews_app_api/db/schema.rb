d68f5504e959ee14d6e72b33dea7b71fc2be9b07
Tiffany Monroe <hello@tiffanymonroe.com>
2017-09-06 18:42:24 -0700

    Commit 3: Products and Reviews associated

--- unit_4/w11d03/homework/products_reviews_app/products_reviews_app_api/db/schema.rb
+++ unit_4/w11d03/homework/products_reviews_app/products_reviews_app_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907012209) do
+ActiveRecord::Schema.define(version: 20170907013410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20170907012209) do
     t.string "img"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "product_id"
   end
 
 end
