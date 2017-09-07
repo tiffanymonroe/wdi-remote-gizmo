2125f3093512651341012a485d5100720e06bed7
Tiffany Monroe <hello@tiffanymonroe.com>
2017-09-06 18:48:23 -0700

    Commit 5: Routes trimmed with only and except

--- unit_4/w11d03/homework/products_reviews_app/products_reviews_app_api/config/routes.rb
+++ unit_4/w11d03/homework/products_reviews_app/products_reviews_app_api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :products do
-    resources :reviews
+  resources :products, only: [:index, :show] do
+    resources :reviews, except: [:show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
