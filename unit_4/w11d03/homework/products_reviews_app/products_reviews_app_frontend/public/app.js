af5c6d885d03b13a44b9b6ace4a4d2628284a67c
Tiffany Monroe <hello@tiffanymonroe.com>
2017-09-06 20:54:20 -0700

    Commit 10: AJAX request to Products Index

--- unit_4/w11d03/homework/products_reviews_app/products_reviews_app_frontend/public/app.js
+++ unit_4/w11d03/homework/products_reviews_app/products_reviews_app_frontend/public/app.js
@@ -1,1 +1,20 @@
-console.log('do you want to build a snowman?');
+// console.log('do you want to build a snowman?');
+
+const app = angular.module('product-reviews', []);
+
+app.controller('mainController', ['$http', function($http){
+
+  this.message = 'under control!';
+  this.products = [];
+  $http({
+    method: 'get',
+    url: 'http://localhost:3000/products',
+  }).then(res => {
+
+    this.products = res.data
+    console.log(this.products);
+
+
+  }).catch(err => console.log(err))
+
+}]); //end controller
